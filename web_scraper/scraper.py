78729f58975c21107649b248e791d08a1b6d52cd
Salim Hassouneh <salim.hassouneh@gmail.com>
2022-05-18 13:28:28 +0300

    lab solution
    created functions to solve the lab

--- web_scraper/scraper.py
+++ web_scraper/scraper.py
@@ -1,8 +1,27 @@
 import requests
-
-URL = "https://en.wikipedia.org/wiki/Cape_Town"
-res = requests.get(URL)
-
 from bs4 import BeautifulSoup
-soup = BeautifulSoup(res.content, "html.parser")
 
+
+def get_citations_needed_count(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    elements = soup.findAll("a", title="Wikipedia:Citation needed")
+    return len(elements)
+
+
+def get_citations_needed_report(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    elements = soup.findAll('p')
+    result = ""
+    for i in elements:
+        citation_exist = i.findAll('a', title="Wikipedia:Citation needed")
+        if citation_exist:
+            result += f'Citation needed for : {i.text}'
+            result += '\n'
+    return result
+
+
+if __name__ == '__main__':
+    print(get_citations_needed_count('https://en.wikipedia.org/wiki/History_of_Mexico'))
+    print(get_citations_needed_report('https://en.wikipedia.org/wiki/History_of_Mexico'))
